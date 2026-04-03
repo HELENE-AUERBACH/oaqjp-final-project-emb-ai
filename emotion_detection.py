0fa15fa346f9c7c3c8687f2d4e6617d015d2a662
HELENE-AUERBACH <82020387+HELENE-AUERBACH@users.noreply.github.com>
2026-04-04 01:32:04 +0200

    Task 2: Create an emotion detection application using the Watson NLP library

---
+++ emotion_detection.py
@@ -0,0 +1,19 @@
+'''
+An emotion detection application using the Watson NLP library
+'''
+import requests # Import the requests library to handle HTTP requests
+
+def emotion_detector(text_to_analyse):
+    '''
+    A function that takes a string input (text_to_analyse)
+    '''
+    # URL of the emotion predict service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/'
+    url += 'NlpService/EmotionPredict'
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json = myobj, headers=header, timeout=5)
+    return response.text # Return the response text from the API
