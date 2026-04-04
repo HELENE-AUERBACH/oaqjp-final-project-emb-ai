ced54edff75ff8d4fd020100b1bd19c7ff67f314
HELENE-AUERBACH <82020387+HELENE-AUERBACH@users.noreply.github.com>
2026-04-04 02:25:08 +0200

    Task 3: Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,8 @@
 '''
 An emotion detection application using the Watson NLP library
 '''
+import json
+import operator
 import requests # Import the requests library to handle HTTP requests
 
 def emotion_detector(text_to_analyse):
@@ -16,4 +18,15 @@ def emotion_detector(text_to_analyse):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json = myobj, headers=header, timeout=5)
-    return response.text # Return the response text from the API
+    #return response.text # Return the response text from the API
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+    #return formatted_response
+    # Extracting required set of emotions,
+    # including anger, disgust, fear, joy and sadness,
+    # along with their scores from the resp
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion_key = max(emotions.items(), key=operator.itemgetter(1))[0]
+    emotions['dominant_emotion'] = dominant_emotion_key
+    return emotions
+
